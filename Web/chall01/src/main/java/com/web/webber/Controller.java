71b64e22c23a3687cba78c218785b3a01642e338
Ipsita Mishra <ipsitamishra16893@gmail.com>
2022-08-31 15:41:14 +0200

    added robots

--- Web/chall01/src/main/java/com/web/webber/Controller.java
+++ Web/chall01/src/main/java/com/web/webber/Controller.java
@@ -1,9 +1,16 @@
 package com.web.webber;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
+	@Value("${f1}")
+	private String f1;
 
 	@GetMapping("/")
 	public String landing(Model model) {
@@ -12,6 +19,13 @@ public class Controller {
 		model.addAttribute("welcome", welcome);
 		model.addAttribute("para", para);
 		return "webber_home";
+	}
+
+	@GetMapping("/robots.txt")
+	public String robots(Model model) {
+
+		model.addAttribute("f1", Arrays.toString(f1.getBytes(StandardCharsets.US_ASCII)));
+		return "robots";
 	}
 
 }
