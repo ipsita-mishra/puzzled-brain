7508c9c6b7f6c3faa8233333cfd7cc6d3a4d944b
Ipsita Mishra <ipsitamishra16893@gmail.com>
2022-09-03 07:36:03 +0200

    Update WebberApplication.java

--- Web/chall01/src/main/java/com/web/webber/WebberApplication.java
+++ Web/chall01/src/main/java/com/web/webber/WebberApplication.java
@@ -2,6 +2,8 @@ package com.web.webber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WebberApplication extends SpringBootServletInitializer {
