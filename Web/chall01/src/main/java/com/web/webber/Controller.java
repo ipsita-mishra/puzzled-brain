9c1f01db93a0c1173edd1059d05a2980b5cd1d76
Ipsita Mishra <ipsitamishra16893@gmail.com>
2022-09-03 07:36:08 +0200

    Update Controller.java

--- Web/chall01/src/main/java/com/web/webber/Controller.java
+++ Web/chall01/src/main/java/com/web/webber/Controller.java
@@ -7,9 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.stereotype.Controller;
 
-@Controller
+@org.springframework.stereotype.Controller
 public class Controller {
 
 	@Value("${f1}")
