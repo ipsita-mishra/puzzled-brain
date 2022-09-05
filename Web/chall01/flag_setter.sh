6c0ee1bfe1db9360d5a8c798992e39bf935a5369
Ipsita Mishra <ipsitamishra16893@gmail.com>
2022-09-05 14:11:00 +0200

    updates

--- Web/chall01/flag_setter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo export ROBOTS=\"$( cat flag1.txt )\" >> ~/.bashrc
-echo export BEANS=\"$( cat flag2.txt )\" >> ~/.bashrc
-source ~/.bashrc
-echo export ROBOTS=\"$( cat flag1.txt )\" >> /etc/environment
-echo export BEANS=\"$( cat flag2.txt )\" >> /etc/environment
-source /etc/environment
