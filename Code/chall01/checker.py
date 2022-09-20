0932fd74e8001b0f746a314fe6d54b20ba6f3e8f
Ipsita Mishra <ipsitamishra16893@gmail.com>
2022-09-20 16:49:39 +0200

    Update checker.py

--- Code/chall01/checker.py
+++ Code/chall01/checker.py
@@ -2,9 +2,10 @@
 # A check function is a Python 2.7 function taking a single argument. The argument provided will be the user's input. The function should then return a Boolean indicating whether the user was correct or not.
 
 def check(x):
-    useradd = "RUN useradd "
-    user = "USER "
-    if (useradd and user in x) and (x.find(useradd) < x.find(user)):
+    useradd = " useradd "
+    user = "user "
+    lowerX = x.lower()
+    if (useradd and user in lowerX) and (lowerX.find(useradd) < lowerX.find(user)):
         return True
     else: 
         return False
